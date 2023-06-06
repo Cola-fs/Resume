f5504b25078f3179fa7bb1248f2f640ce6248d73
Cola-fs <lobur3n@gmail.com>
2023-06-06 22:39:07 +0300

    first commit

--- src/route/index.js
+++ src/route/index.js
@@ -23,6 +23,27 @@ router.get('/summary', function (req, res) {
   //             ↙ cюди вводимо назву файлу з сontainer
   res.render('summary', {
     // ↙ сюди вводимо JSON дані
+
+    name: {
+      firstname: 'Ivan',
+      lastname: 'Ivanov',
+    },
+    salary: '600$',
+    position: 'Junior Fullstack JS Developer',
+    social: {
+      email: {
+        text: 'ivanov@gmail.com',
+        href: 'mailto:ivanov@gmail.com',
+      },
+    },
+    summary: {
+      title: 'Summary',
+      text: 'Open-minded for new technologies, with 1 years of experience in development. Whenever I start to       work on a new project I learn the domain and try to understand the idea of the project. Good team player, every colleague is a friend to me.',
+    },
+    experience: {
+      title: 'Other experience',
+      text: 'Pet project for parsing sport betting data from different platforms ( odds ) and sport statistics (tournament position, goals etc), analyzing by simple mathematics models and preparing probability for such events like: money line - first win / draw / second win, totals etc.'
+    },
   })
 })
 
