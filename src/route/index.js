78b74e69c0a77cb90761c51bfe09e0d369ff4e20
Cola-fs <132342394+Cola-fs@users.noreply.github.com>
2023-06-08 22:51:30 +0300

    final

--- src/route/index.js
+++ src/route/index.js
@@ -9,101 +9,143 @@ const router = express.Router()
 
 //           ↙ тут вводимо шлях (PATH) до сторінки
 router.get('/', function (req, res) {
-  // res.render генерує нам HTML сторінку
+	// res.render генерує нам HTML сторінку
 
-  //            ↙ cюди вводимо назву файлу з сontainer
-  res.render('index', {})
-  //                  ↑↑ сюди вводимо JSON дані
+	//            ↙ cюди вводимо назву файлу з сontainer
+	res.render('index', {})
+	//                  ↑↑ сюди вводимо JSON дані
 })
 
 // ================================================================
 
 //              ↙ тут вводимо шлях (PATH) до сторінки
 router.get('/summary', function (req, res) {
-  //             ↙ cюди вводимо назву файлу з сontainer
-  res.render('summary', {
-    // ↙ сюди вводимо JSON дані
-    page: {
-      title: 'Resume',
-    },
-    header: {
-      name: {
-        firstname: 'Ivan',
-        lastname: 'Ivanov',
-      },
-      position: 'Junior Fullstack JS Developer',
-      salary: '600$',
-    },
-
-    footer: {
-      social: {
-        email: {
-          text: 'ivanov@gmail.com',
-          href: 'mailto:ivanov@gmail.com',
-        },
-        phone: {
-          text: '+380670000123',
-          href: 'tel:+380670000123',
-        },
-        LinkedIn: {
-          text: 'LinkedIn',
-          href: 'https://www.linkedin.com/in/dmytro-test',
-        },
-      },
-    },
-
-    main: {
-      summary: {
-        title: 'Summary',
-        text: 'Open-minded for new technologies, with 1 years of experience in development. Whenever I start to       work on a new project I learn the domain and try to understand the idea of the project. Good team player, every colleague is a friend to me.',
-      },
-      experience: {
-        title: 'Other experience',
-        text: 'Pet project for parsing sport betting data from different platforms ( odds ) and sport statistics (tournament position, goals etc), analyzing by simple mathematics models and preparing probability for such events like: money line - first win / draw / second win, totals etc.',
-      },
-    },
-  })
+	//             ↙ cюди вводимо назву файлу з сontainer
+	res.render('summary', {
+		// ↙ сюди вводимо JSON дані
+		page: {
+			title: 'Resume',
+		},
+		header: {
+			name: {
+				firstname: 'Ivan',
+				lastname: 'Ivanov',
+			},
+			position: 'Junior Fullstack JS Developer',
+			salary: '600$',
+			address: 'Ukraine, Vinnitsa region',
+		},
+		footer: {
+			social: {
+				email: {
+					text: 'ivanov@gmail.com',
+					href: 'mailto:ivanov@gmail.com',
+				},
+				phone: {
+					text: '+380670000123',
+					href: 'tel:+380670000123',
+				},
+				LinkedIn: {
+					text: 'LinkedIn',
+					href: 'https://www.linkedin.com/in/dmytro-test',
+				},
+			},
+		},
+		main: {
+			summary: {
+				title: 'Summary',
+				text: 'Open-minded for new technologies, with 1 years of experience in development. Whenever I start to       work on a new project I learn the domain and try to understand the idea of the project. Good team player, every colleague is a friend to me.',
+			},
+			experience: {
+				title: 'Other experience',
+				text: 'Pet project for parsing sport betting data from different platforms ( odds ) and sport statistics (tournament position, goals etc), analyzing by simple mathematics models and preparing probability for such events like: money line - first win / draw / second win, totals etc.',
+			},
+		},
+	})
 })
-
 // ================================================================
 //              ↙ тут вводимо шлях (PATH) до сторінки
 router.get('/skills', function (req, res) {
-  //             ↙ cюди вводимо назву файлу з сontainer
-  res.render('skills', {
-    // ↙ сюди вводимо JSON дані
-    page: {
-      title: 'Resume',
-    },
-    header: {
-      name: {
-        firstname: 'Ivan',
-        lastname: 'Ivanov',
-      },
-      position: 'Junior Fullstack JS Developer',
-      salary: '600$',
-    },
+	//             ↙ cюди вводимо назву файлу з сontainer
+	res.render('skills', {
+		// ↙ сюди вводимо JSON дані
+		page: {
+			title: 'Resume',
+		},
+		header: {
+			name: {
+				firstname: 'Ivan',
+				lastname: 'Ivanov',
+			},
+			position: 'Junior Fullstack JS Developer',
+			salary: '600$',
+			address: 'Ukraine, Vinnitsa region',
+		},
+		footer: {
+			social: {
+				email: {
+					text: 'ivanov@gmail.com',
+					href: 'mailto:ivanov@gmail.com',
+				},
+				phone: {
+					text: '+380670000123',
+					href: 'tel:+380670000123',
+				},
+				LinkedIn: {
+					text: 'LinkedIn',
+					href: 'https://www.linkedin.com/in/dmytro-test',
+				},
+			},
+		},
 
-    footer: {
-      social: {
-        email: {
-          text: 'ivanov@gmail.com',
-          href: 'mailto:ivanov@gmail.com',
-        },
-        phone: {
-          text: '+380670000123',
-          href: 'tel:+380670000123',
-        },
-        LinkedIn: {
-          text: 'LinkedIn',
-          href: 'https://www.linkedin.com/in/dmytro-test',
-        },
-      },
-    },
+		main: {
+			skills: ['HTML', 'Handlebars', 'VS Code', 'Git', 'Terminal', 'NPM',],
 
-    main: {},
-  })
+
+		},
+	})
 })
 
+//================================================================
+//              ↙ тут вводимо шлях (PATH) до сторінки
+router.get('/education', function (req, res) {
+	//             ↙ cюди вводимо назву файлу з сontainer
+	res.render('education', {
+		// ↙ сюди вводимо JSON дані
+		page: {
+			title: 'Resume',
+		},
+		header: {
+			name: {
+				firstname: 'Ivan',
+				lastname: 'Ivanov',
+			},
+			position: 'Junior Fullstack JS Developer',
+			salary: '600$',
+			address: 'Ukraine, Vinnitsa region',
+		},
+		footer: {
+			social: {
+				email: {
+					text: 'ivanov@gmail.com',
+					href: 'mailto:ivanov@gmail.com',
+				},
+				phone: {
+					text: '+380670000123',
+					href: 'tel:+380670000123',
+				},
+				LinkedIn: {
+					text: 'LinkedIn',
+					href: 'https://www.linkedin.com/in/dmytro-test',
+				},
+			},
+		},
+
+		main: {
+			Educations: ['Scholl', 'Univer', 'Kursi'],
+		},
+	})
+})
 // ================================================================
 
 // Підключаємо роутер до бек-енду
