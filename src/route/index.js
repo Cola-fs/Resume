922f717f8b0dfbb67d043af6d17ad72db82d3ca0
Cola-fs <132342394+Cola-fs@users.noreply.github.com>
2023-06-08 21:46:43 +0300

    “info”

--- src/route/index.js
+++ src/route/index.js
@@ -23,27 +23,84 @@ router.get('/summary', function (req, res) {
   //             ↙ cюди вводимо назву файлу з сontainer
   res.render('summary', {
     // ↙ сюди вводимо JSON дані
-
-    name: {
-      firstname: 'Ivan',
-      lastname: 'Ivanov',
+    page: {
+      title: 'Resume',
     },
-    salary: '600$',
-    position: 'Junior Fullstack JS Developer',
-    social: {
-      email: {
-        text: 'ivanov@gmail.com',
-        href: 'mailto:ivanov@gmail.com',
+    header: {
+      name: {
+        firstname: 'Ivan',
+        lastname: 'Ivanov',
+      },
+      position: 'Junior Fullstack JS Developer',
+      salary: '600$',
+    },
+
+    footer: {
+      social: {
+        email: {
+          text: 'ivanov@gmail.com',
+          href: 'mailto:ivanov@gmail.com',
+        },
+        phone: {
+          text: '+380670000123',
+          href: 'tel:+380670000123',
+        },
+        LinkedIn: {
+          text: 'LinkedIn',
+          href: 'https://www.linkedin.com/in/dmytro-test',
+        },
       },
     },
-    summary: {
-      title: 'Summary',
-      text: 'Open-minded for new technologies, with 1 years of experience in development. Whenever I start to       work on a new project I learn the domain and try to understand the idea of the project. Good team player, every colleague is a friend to me.',
+
+    main: {
+      summary: {
+        title: 'Summary',
+        text: 'Open-minded for new technologies, with 1 years of experience in development. Whenever I start to       work on a new project I learn the domain and try to understand the idea of the project. Good team player, every colleague is a friend to me.',
+      },
+      experience: {
+        title: 'Other experience',
+        text: 'Pet project for parsing sport betting data from different platforms ( odds ) and sport statistics (tournament position, goals etc), analyzing by simple mathematics models and preparing probability for such events like: money line - first win / draw / second win, totals etc.',
+      },
     },
-    experience: {
-      title: 'Other experience',
-      text: 'Pet project for parsing sport betting data from different platforms ( odds ) and sport statistics (tournament position, goals etc), analyzing by simple mathematics models and preparing probability for such events like: money line - first win / draw / second win, totals etc.'
+  })
+})
+
+// ================================================================
+//              ↙ тут вводимо шлях (PATH) до сторінки
+router.get('/skills', function (req, res) {
+  //             ↙ cюди вводимо назву файлу з сontainer
+  res.render('skills', {
+    // ↙ сюди вводимо JSON дані
+    page: {
+      title: 'Resume',
     },
+    header: {
+      name: {
+        firstname: 'Ivan',
+        lastname: 'Ivanov',
+      },
+      position: 'Junior Fullstack JS Developer',
+      salary: '600$',
+    },
+
+    footer: {
+      social: {
+        email: {
+          text: 'ivanov@gmail.com',
+          href: 'mailto:ivanov@gmail.com',
+        },
+        phone: {
+          text: '+380670000123',
+          href: 'tel:+380670000123',
+        },
+        LinkedIn: {
+          text: 'LinkedIn',
+          href: 'https://www.linkedin.com/in/dmytro-test',
+        },
+      },
+    },
+
+    main: {},
   })
 })
 
